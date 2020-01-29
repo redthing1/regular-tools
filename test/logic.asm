6951f05ed483bd18493d67379d687c41c09d436c
xdrie <xdrie@alt.icu>
2020-01-29 13:56:09 -0800

    add a logic test program

--- test/logic.asm
+++ test/logic.asm
@@ -2,8 +2,18 @@
 
 #entry :main
 
+cond1:
+    set r2 $00ff
+    int r2 ; raise r2
+    jmi ::test2 ; resume the test
+
 main:
+test1:
+    ; test conditional jumping
     set r1 .10
-    set r2 .01
-    swp r1 r2
+    set r2 .10
+    ; compare values
+    tcu r3 r1 r2
+test2:
+    
     hlt
