05857b4d4867304cac9c1e0d72c665aa2502792b
xdrie <xdrie@alt.icu>
2020-01-29 18:12:57 -0800

    fix func program

--- test/func.asm
+++ test/func.asm
@@ -4,18 +4,22 @@
 
 sub:
     set r2 $0069 ; return value
-    mov r3 sp ; retval dest [sp-4]
+    ; retval dest is [sp-4]
     set at $4
-    sub r3 r3 at
+    sub r3 sp at
     stw r3 r2 ; store retval
     set r4 $01
-    int r4
     ret
 
 main:
     set r1 $0041 ; arg1
     psh r1
     set r4 ::sub
-    cal r4
-    pop r1 ; pop arg1
-    hlt
+    cal r4 ; call the subprocedure
+    int r4 ; raise interrupt
+    ; get the return value from [sp-8]
+    set at $8
+    sub r1 sp at
+    ldw r2 r1 ; put the return value into r2
+    pop r1 ; pop arg1 back into r1
+    hlt ; r1 = $41, r2 = $69
