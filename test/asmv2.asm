7f19329bbfac070bb56e017aa081ce4c1f9b4460
xdrie <xdrie@alt.icu>
2020-02-17 10:51:02 -0800

    add v2 notes s1

---
+++ test/asmv2.asm
@@ -0,0 +1,34 @@
+; test of assembly v2
+; v2 REGULAR_ad asm is incompatible with v1
+; it is a revision rewritten for better assembler features
+; the v1 assembler was rushed and a lot of features were bolted on
+
+#entry :main
+; this time, spec compatibility is improved.
+; entry point is stored in a WORD-size slot
+; if the emulator does not support our header
+; we simply insert a jmp
+
+; data must appear before code
+data0:
+    #d \x22000000 ; $22 in little endian
+data1:
+    #d \'hello ; data string support
+    #d \x00 ; null terminator
+
+addi@ rA rB v0 ; this macro sets rA = rB + v0
+    add rA rB v0
+
+main:
+    set r1 $0
+    set r2 $4
+    @addi r1 r2 $8
+    ; labels can be ahead-referenced
+    ; any relative offsets will be resolved later
+    cal ::func1
+
+    ; labels can be accessed with positive offsets
+    set r3 ::data1.$4 ; pointer to "hello" string
+
+func1:
+    set r7 $0
